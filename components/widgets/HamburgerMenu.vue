649bc6e2dfeb1bb6ce3d165c8b0e44b200a49ffa
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-02-10 00:50:52 +0300

    hero section

---
+++ components/widgets/HamburgerMenu.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="hamburger-menu" @click="toggleMenu()">
+        <div class="hamburger-layer"></div>
+        <div class="hamburger-layer"></div>
+    </div>
+</template>
+
+<script>
+import gsap from 'gsap';
+export default {
+    name: 'HamburgerMenu',
+    methods: {
+        toggleMenu(){
+            let hamburgerAnimation = gsap.timeline();
+            this.$emit('toggleMenu');
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .hamburger-menu{
+        display: none;
+    }
+
+    @media(max-width: 48rem){
+        .hamburger-menu{
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            width: 35px;
+            height: 18px;
+
+           .hamburger-layer{
+               width: 100%;
+               height: 2px;
+               background-color: $clr-dark;
+            }
+        }
+    }
+</style>
--- components/widgets/TheNavigation.vue
+++ components/widgets/TheNavigation.vue
@@ -8,16 +8,69 @@
               <li><a href="work" class="navigation__link">Work</a></li>
               <li><a href="contact" class="navigation__link">Contact</a></li>
           </ul>
+          <hamburger-menu @toggleMenu="toggleMenu"/>
       </div>
   </nav>
 </template>
 
 <script>
+import HamburgerMenu from '~/components/widgets/HamburgerMenu'
+import gsap from 'gsap';
+
 export default {
-    name: 'TheNavigation'
+    name: 'TheNavigation',
+    components: {
+        HamburgerMenu
+    },
+    methods: {
+        toggleMenu(){
+            let toggleMenuTl = gsap.timeline();
+            let menu = document.querySelector('.navigation__links');
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    nav{
+    position: absolute;
+    width: 100%;
+    height: 12vh;
 
+    .navigation{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        
+        .logo{
+            
+        }
+        
+        .navigation__links{
+            display: flex;
+            align-items: center;
+            flex-direction: row;
+            gap: 2em;
+        }
+    }
+    }
+    @media(max-width: 48rem){
+        nav{
+            .navigation{
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: space-between;
+                
+                .logo{
+                    
+                }
+                
+                .navigation__links{
+                    display: none;
+                }
+            }
+        }
+    }
 </style>
