6dd7dc65af53d7ecfa56a89f432612c551bfd9a2
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-06-26 14:50:39 +0300

    feat: mobile cards link icon

--- components/miscellaneous/Card.vue
+++ components/miscellaneous/Card.vue
@@ -23,7 +23,18 @@
              
         </figure>
         <div data-scroll data-scroll-speed="3" class="item__text">
-            <h2 class="heading">{{ work.title }}</h2>
+            <div class="item-text__header">
+                <h2 class="heading">{{ work.title }}</h2>
+                <svg viewBox="0 0 128 128">
+                    <g>
+                        <g>
+                            <line style="stroke-linecap:square;stroke-miterlimit:10;" x1="42.288" x2="105.999" y1="22" y2="22.001"/>
+                            <line style="stroke-linecap:square;stroke-miterlimit:10;" x1="105.999" x2="106.001" y1="22.001" y2="85.713"/>
+                        </g>
+                        <line style="stroke-miterlimit:10;" x1="105.999" x2="15.999" y1="22.001" y2="112"/>
+                    </g>
+                </svg>
+            </div>
             <ul>
                 <li
                 v-for="(tech, i) in work.techStack"
@@ -81,6 +92,16 @@ export default {
             max-width: 50%;
             z-index: 2;
 
+            .item-text__header{
+                display: flex;
+                width: 100%;
+                justify-content: space-between;
+
+                svg{
+                    display: none;
+                }
+            }
+
             ul{
                 display: flex;
                 flex-wrap: 1;
@@ -105,7 +126,7 @@ export default {
         }
     }
 
-    @media(max-width: 48rem){
+    @media(max-width: 62rem){
         .cards__item{
             figure{
                 width: 100%;
@@ -116,6 +137,24 @@ export default {
                 top: 0;
                 transform: translateY(0%);
                 max-width: 100%;
+
+                .item-text__header{
+                    display: flex;
+                    width: 100%;
+                    justify-content: space-between;
+        
+                    svg{
+                        display: block;
+                        width: 30px;
+                        height: 30px;
+
+                        *{
+                            stroke: $clr-dark;
+                            fill: none;
+                            stroke-width: 4;
+                        }
+                    }
+                }
 
                 .heading{
                     font-size: var(--fs-l);
