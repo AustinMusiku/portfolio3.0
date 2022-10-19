dc709094ed109342d697203a2db4f05534f58887
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-10-20 02:42:41 +0300

    feat: improved footer section ui

--- components/sections/TheFooter.vue
+++ components/sections/TheFooter.vue
@@ -1,13 +1,13 @@
 <template>
   <footer data-scroll-section class="grid">
-    <div class="grid__container">
+    <div class="grid__container footer_block">
       <ul class="social">
         <li><a target="_blank" rel="noopener" href="https://twitter.com/MusikuAustin/" class="underline social__link">Twitter</a></li>
         <li><a target="_blank" rel="noopener" href="https://github.com/AustinMusiku/" class="underline social__link">Github</a></li>
         <!-- <li><a target="_blank" rel="noopener" href="https://codepen.io/austinmusiku" class="underline social__link">Codepen</a></li> -->
         <li><a target="_blank" rel="noopener" href="https://www.linkedin.com/in/austin-musiku-0b37b819b/" class="underline social__link">LinkedIn</a></li>
       </ul>
-      <p>Austin Musiku 2022</p>
+      <p class="copyright">Austin Musiku 2022</p>
     </div>
   </footer>
 </template>
@@ -29,9 +29,8 @@ export default {
       justify-content: space-between;
       align-items: center;
 
-
       .social{
-        padding: 2em 0;
+        padding: 1.5em 0;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
@@ -47,11 +46,18 @@ export default {
       .grid__container{
         padding: 1.5em;
         flex-direction: column;
+        justify-self: center;
         align-items: flex-start;
         
         .social{
-          padding: 0 0 .5em;
-          gap: .75em;
+          padding: 0 0 1em;
+          gap: 2em;
+          max-width: 20em;
+        }
+
+        .copyright{
+          text-align: center;
+          width: 100%;
         }
       }
   }
