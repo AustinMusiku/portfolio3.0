0009645d12d4f6db62812ea43b3f670ef8c248e9
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-01-10 03:06:11 +0300

    feat: updated hero section

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -1,44 +1,43 @@
 <template>
-    <div class="hero grid" :class="{ 'hero--fullscreen' : fullScreen }">
+    <div class="hero grid">
         <div class="grid__container hero__content">
             <slot name="hero-content"></slot>
         </div>
     </div>
 </template>
 
-<script>
-export default {
-    name: 'Hero',
-    props: { 
-        fullScreen: {
-            type: Boolean,
-            default: false
-        }
-    }
-}
-</script>
-
 <style lang="scss" scoped>
     .hero{
         align-items: end;
-        height: 80vh;
-        // background: $clr-grey;
-
-        &.hero--fullscreen{
-            height: 100vh;
-            // background: url('/images/noise.png') no-repeat center center;
-        }
+        height: 100vh;
 
         .hero__content{
-            margin: 0 0 4em;
+            margin: 0 0 2.5em;
         }
     }
+
+    @media(max-width: 90rem){
+        .hero{
+            .hero__content{
+                margin: 0 0 2.5em;
+            }
+        }
+    }
+
+    @media(min-width: 90rem){
+        .hero{
+            .hero__content{
+                margin: 0 0 4em;
+            }
+        }
+    }
+
     @media(max-width: 48rem){
         .hero{
             padding: 0 1.5em;
 
             .hero__content{
-                margin: 0 0 5em;
+                margin: 0 0 1.5em;
                 padding: 0;
             }
         }
@@ -47,6 +46,11 @@ export default {
     @media(max-width: 20rem){
         .hero{
             padding: 0 1em;
+
+            .hero__content{
+                margin: 0 0 1em;
+                padding: 0;
+            }
         }
     }
 </style>
