4ebf95950be1a643ccb4f64b35dbb2c76613d391
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-07-04 04:00:28 +0300

    fix: tweaked curtains easings

--- components/sections/Curtain.vue
+++ components/sections/Curtain.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="curtains" ref="curtains">
-        <div class="curtain curtain__inner" ref="curtain__inner">
-            
-        </div>
+        <div class="curtain curtain__inner" ref="curtain__inner"></div>
 
         <div class="curtain curtain__outer" ref="curtain__outer">
             <div class="curtain__content">
@@ -26,158 +24,155 @@
                     </div>
                 </div>
             </div>
-
         </div>
-
     </div>
 </template>
 
 <script>
-    import { gsap } from "gsap";
+import { gsap } from "gsap";
 
-    export default {
-        name: 'Curtain',
-        data(){
-            return{
-                isOpen: false
-            }
+export default {
+    name: "Curtain",
+    data() {
+        return {
+            isOpen: false,
+        };
+    },
+    methods: {
+        fade() {
+            let tl = gsap.timeline();
+
+            window.addEventListener("load", () => {
+                tl.to(".letter", {
+                    opacity: 1,
+                    duration: 0.65,
+                    ease: "power1.inout",
+                    stagger: 0.025,
+                }).to([".curtain__outer", ".curtain__inner"], {
+                    duration: 2.5,
+                    y: "-130%",
+                    ease: "power4.inOut",
+                    stagger: 0.25,
+                });
+            });
         },
-        methods: {
-            fade(){
-                let tl = gsap.timeline();
-
-                window.addEventListener('load', () => {
-                    tl
-                        .to('.letter', {
-                            opacity: 1,
-                            duration: .65,
-                            ease: 'power1.inout',
-                            stagger: 0.025
-                        })
-
-                        .to(['.curtain__outer', '.curtain__inner'], {
-                            duration: 2,
-                            y: '-130%',
-                            ease: 'power3.inOut',
-                            stagger: 0.125
-                        })
-                })
-            }
-        },
-        mounted(){
-            this.fade();
-        }
-    }
+    },
+    mounted() {
+        this.fade();
+    },
+};
 </script>
 
 <style lang="scss" scoped>
-    .curtain{
-        position: fixed;
-        display: grid;
-        place-items: center;
-        top: 0;
-        left: 0;
-        width: 100vw;
-        height: 100vh;
-        z-index: 20000;
-        
-        &.curtain__outer,&.curtain__inner{
-            .loader{
-                left: 0;
-                position: absolute;
-                bottom: 0%;
-                width: 100vw;
-                height: 16vh;
+.curtain {
+    position: fixed;
+    display: grid;
+    place-items: center;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 20000;
 
-                @media(max-width: 48rem){
-                    .loader{
-                        height: 10vh !important;
-                    }
-                }
-            }
-        }
-
-        &.curtain__outer{
-            background: $clr-light;
-            transform-origin: bottom;
-
-            .loader{
-                transform: translateY(98%);
-            }
-        }
-        &.curtain__inner{
-            background: #8fd589;
-            height: 200vh;
-            .loader{
-                transform: translateY(70%);
-            }
-        }
-        
-        .curtain__content{
-            padding: 4em;
+    &.curtain__outer,
+    &.curtain__inner {
+        .loader {
+            left: 0;
+            position: absolute;
+            bottom: 0%;
             width: 100vw;
+            height: 16vh;
 
-            .names{
-                position: relative;
-                display: flex;
-                flex-direction: row;
-                justify-content: center;
-                gap: 2em;
+            @media (max-width: 48rem) {
+                .loader {
+                    height: 10vh !important;
+                }
+            }
+        }
+    }
+
+    &.curtain__outer {
+        background: $clr-light;
+        transform-origin: bottom;
+
+        .loader {
+            transform: translateY(98%);
+        }
+    }
+    &.curtain__inner {
+        background: #8fd589;
+        height: 200vh;
+        .loader {
+            transform: translateY(70%);
+        }
+    }
+
+    .curtain__content {
+        padding: 4em;
+        width: 100vw;
+
+        .names {
+            position: relative;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            gap: 2em;
+            width: 100%;
+            --scale: 0;
+            --origin: left;
+
+            .name {
+                .letter {
+                    text-transform: capitalize;
+                    font-size: var(--fs-xl);
+                    font-weight: $fw-bold;
+                    opacity: 0;
+                }
+            }
+
+            &::before,
+            &::after {
+                content: "";
+                position: absolute;
+                left: 0;
+                height: 1px;
                 width: 100%;
-                --scale: 0;
-                --origin: left;
+                background-color: #4f4f4f;
+                transform: scaleX(var(--scale));
+                transform-origin: var(--origin);
+            }
 
-                .name{
-                    .letter{
-                        text-transform: capitalize;
-                        font-size: var(--fs-xl);
-                        font-weight: $fw-bold;
-                        opacity: 0;
-                    }
-                }
-
-                &::before, 
-                &::after{
-                    content: '';
-                    position: absolute;
-                    left: 0;
-                    height: 1px;
-                    width: 100%;
-                    background-color: #4f4f4f;
-                    transform: scaleX(var(--scale));
-                    transform-origin: var(--origin);
-                }
-
-                &::before{
-                    top: -2.5px;
-                }
-                &::after{
-                    bottom: -2.5px;
-                }
+            &::before {
+                top: -2.5px;
+            }
+            &::after {
+                bottom: -2.5px;
             }
         }
     }
+}
 
-    @media(max-width: 48rem){
-        .curtain{
-            &.curtain__outer,&.curtain__inner{
-                .loader{
-                    height: 10vh;
-                }
+@media (max-width: 48rem) {
+    .curtain {
+        &.curtain__outer,
+        &.curtain__inner {
+            .loader {
+                height: 10vh;
             }
+        }
 
-            .curtain__content{
-                padding: 1em;
-                .names{
-                    gap: 1em;
+        .curtain__content {
+            padding: 1em;
+            .names {
+                gap: 1em;
 
-                    .name{
-                        .letter{
-                            font-size: var(--fs-l);
-                        }
+                .name {
+                    .letter {
+                        font-size: var(--fs-l);
                     }
                 }
             }
         }
     }
+}
 </style>
